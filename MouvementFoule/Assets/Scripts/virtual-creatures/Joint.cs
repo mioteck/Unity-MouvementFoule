f5b5448f217abcdff6a0247df3d74b443ba0692c
Wh1t3L0tus <wh1t3l07u5@gmail.com>
2017-03-09 11:55:57 +0100

    update Joint

--- MouvementFoule/Assets/Scripts/virtual-creatures/Joint.cs
+++ MouvementFoule/Assets/Scripts/virtual-creatures/Joint.cs
@@ -1,10 +1,73 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
-public class Joint{
+public class Joint : ICloneable
+{
+    public BodyPart parent;
+    public BodyPart child;
 
+    public Vector3 parentAnchor;
+    public Vector3 childAnchor;
 
+    public Joint(BodyPart parent, BodyPart child)
+    {
+        this.parent = parent;
+        this.child = child;
 
+        parentAnchor = new Vector3(RandomAnchor(), RandomAnchor(), RandomAnchor());
+        childAnchor = new Vector3(RandomAnchor(), RandomAnchor(), RandomAnchor());
+    }
 
+    public Joint(BodyPart parent, BodyPart child, Vector3 parentAnchor, Vector3 childAnchor)
+    {
+        this.parent = parent;
+        this.child = child;
+        this.parentAnchor = parentAnchor;
+        this.childAnchor = childAnchor;
+    }
+
+    public Joint(Joint j)
+    {
+        parent = new BodyPart(j.parent);
+        child = new BodyPart(j.child);
+        parentAnchor = j.parentAnchor;
+        childAnchor = j.childAnchor;
+    }
+
+    public BodyPart GetParent()
+    {
+        return parent;
+    }
+
+    public BodyPart GetChild()
+    {
+        return child;
+    }
+
+    public Vector3 GetParentAnchor()
+    {
+        return parentAnchor;
+    }
+
+    public Vector3 GetChildAnchor()
+    {
+        return childAnchor;
+    }
+
+    private float RandomAnchor()
+    {
+        int val = Random.Range(0, 1);
+        if (val == 0)
+        {
+            return -1;
+        }
+
+        return 1;
+    }
+
+    public object Clone()
+    {
+        return new Joint(parent, child, parentAnchor, childAnchor);
+    }
 }
