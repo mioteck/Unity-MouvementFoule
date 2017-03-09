2ede3edd08b45ab880be79f2926be9f20b3ae27d
Unknown <nelson.defoulny@gmail.com>
2017-03-09 11:10:59 +0100

    Part 2 init

---
+++ MouvementFoule/Assets/Scripts/virtual-creatures/DNAMonster.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class DNAMonster{
+    private BodyPart bodyPart;
+    private DNAMonster[] children;
+    private Joint[] joints;
+
+
+
+}
