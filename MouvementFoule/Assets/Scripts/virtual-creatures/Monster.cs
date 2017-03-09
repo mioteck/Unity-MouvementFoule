94f623647db2f80c2e1496ecb47a666947a63e84
Unknown <nelson.defoulny@gmail.com>
2017-03-09 12:32:11 +0100

    add class for monster

---
+++ MouvementFoule/Assets/Scripts/virtual-creatures/Monster.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour {
+    public DNAMonster dna;
+    public GameObject[] go;
+    public int score;
+    public Vector3 startPos, endPos;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+}
