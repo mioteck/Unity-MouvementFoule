7df0bb827f129f58f1320c4e0c4e7b585c1296b0
Wh1t3L0tus <wh1t3l07u5@gmail.com>
2017-03-15 02:49:17 +0100

    feat(DNA serializer): add a DNA serializer
    Serialization and deserialization works but instancion
    from deserialized dna fails

---
+++ MouvementFoule/Assets/Scripts/virtual-creatures/DNASerializer.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+using UnityEngine;
+
+[Serializable]
+public struct DNAMonsterSer
+{
+    public BodyPart bodyPart;
+    public Vector3 parentAnchor;
+    public int score;
+    public Vector3[] anchors;
+    public DNAMonsterSer[] children;
+}
+
+[Serializable]
+public struct BodyPartSer
+{
+    public Vector3 size;
+}
+
+// Utility class to serialize/deserialize monster's DNA
+public class DNASerializer {
+
+    // DNA -> string
+    public static string serialize(DNAMonster dna)
+    {
+        return JsonUtility.ToJson(translateDNAMonster(dna));
+    }
+
+    // string -> DNA
+    public static DNAMonster deserializeMonster(string dnaStr)
+    {
+        DNAMonsterSer receivedDNA = JsonUtility.FromJson<DNAMonsterSer>(dnaStr);
+
+        return translateDNAMonsterSer(receivedDNA);
+    }
+
+    // DNA -> file
+    public static void writeDNAToFile(DNAMonster dna, string filename)
+    {
+        StreamWriter sr = File.CreateText(filename);
+        sr.Write(serialize(dna));
+        sr.Close();
+    }
+
+    // fileName -> DNA
+    public static DNAMonster loadDNAFromFile(string filename)
+    {
+        if (File.Exists(filename))
+        {
+            string jsonStr = File.ReadAllText(filename);
+            return deserializeMonster(jsonStr);
+        }
+        else
+        {
+            Debug.Log("Cannot find dna file " + filename);
+            return null;
+        }
+    }
+
+    private static DNAMonsterSer translateDNAMonster(DNAMonster dnaMonster)
+    {
+        DNAMonsterSer serDna = new DNAMonsterSer();
+        serDna.bodyPart = dnaMonster.getBodyPart();
+        serDna.parentAnchor = dnaMonster.getParentAnchor();
+        serDna.score = dnaMonster.getScore();
+        serDna.anchors = dnaMonster.getAnchor();
+
+        if (dnaMonster.getChildren() != null)
+        {
+            serDna.children = new DNAMonsterSer[dnaMonster.getChildren().Length];
+
+            for (int i = 0; i < serDna.children.Length; i++)
+            {
+                serDna.children[i] = translateDNAMonster(dnaMonster.getChildren()[i]);
+            }
+        }
+        else
+        {
+            serDna.children = null;
+        }
+
+        return serDna;
+    }
+    
+    private static DNAMonster translateDNAMonsterSer(DNAMonsterSer dna)
+    {
+        return new DNAMonster(dna.bodyPart, dna.children, dna.anchors, dna.parentAnchor);
+    }
+}
---
+++ MouvementFoule/Assets/Scripts/virtual-creatures/SerializeTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SerializeTest : MonoBehaviour {
+
+    public GameObject prefab;
+
+	void Start () {
+
+        DNAMonster dna = new DNAMonster(Vector3.zero, 0);
+
+        GameObject monster = Instantiate(prefab, Vector3.zero, new Quaternion(0, 0, 0, 0));
+        Monster monsterComponent = monster.GetComponent<Monster>();
+        monsterComponent.initMonster(dna, LayerMask.NameToLayer("physx1"));
+
+        string dnaStr1 = DNASerializer.serialize(dna);
+        DNAMonster dnaMonster = DNASerializer.deserializeMonster(dnaStr1);
+        string dnaStr2 = DNASerializer.serialize(dnaMonster);
+
+        Debug.Log("DNA Written == DNA Loaded ===> " + dnaStr1.Equals(dnaStr2));
+
+        DNASerializer.writeDNAToFile(dna, "dna.txt");
+        DNAMonster dnaMonster2 = DNASerializer.loadDNAFromFile("dna.txt");
+
+        GameObject monster2 = Instantiate(prefab, Vector3.zero + 4 * Vector3.right, new Quaternion(0, 0, 0, 0));
+        Monster monsterComp2 = monster2.GetComponent<Monster>();
+        monsterComp2.initMonster(dnaMonster2, LayerMask.NameToLayer("physx1") + 1);
+    }
+}
