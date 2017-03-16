045ca838792703f0b1d7dd4fe1994cdc0341a301
bbousq <bousquieres.benjamin@hotmail.fr>
2017-03-17 00:26:52 +0100

    Final Scene : Creation

---
+++ MouvementFoule/Assets/Scripts/ChangeApp.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeApp : MonoBehaviour {
+    [Header("Boids")]
+    public Button boidsButton;
+    public GameObject boidsPanel;
+    public GameObject boidsCamera;
+    [Space]
+    [Header("Creature Generation")]
+    public Button creatureButton;
+    public GameObject creaturePanel;
+    public GameObject creatureCamera;
+    [Space]
+    [Header("Ants")]
+    public Button antsButton;
+    public GameObject antsPanel;
+    public GameObject antsCamera;
+
+	void Start () {
+        boidsButton.onClick.AddListener(ChangeToBoids);
+        creatureButton.onClick.AddListener(ChangeToCreature);
+        antsButton.onClick.AddListener(ChangeToAnts);
+	}
+	
+    public void ChangeToBoids()
+    {
+        boidsPanel.SetActive(true);
+        creaturePanel.SetActive(false);
+        antsPanel.SetActive(false);
+
+        Camera.main.transform.position = boidsCamera.transform.localPosition;
+    }
+
+    public void ChangeToCreature()
+    {
+        boidsPanel.SetActive(false);
+        creaturePanel.SetActive(true);
+        antsPanel.SetActive(false);
+
+        Camera.main.transform.position = creatureCamera.transform.localPosition;
+    }
+
+    public void ChangeToAnts()
+    {
+        boidsPanel.SetActive(false);
+        creaturePanel.SetActive(false);
+        antsPanel.SetActive(true);
+
+        Camera.main.transform.position = antsCamera.transform.localPosition;
+    }
+}
--- MouvementFoule/Assets/Scripts/ant_GA_scripts/AntController.cs
+++ MouvementFoule/Assets/Scripts/ant_GA_scripts/AntController.cs
@@ -1,41 +1,64 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
-public class AntController : MonoBehaviour {
+public class AntController : MonoBehaviour
+{
     public GameObject SPIDER;
-    
-    private static List<Path> path;
-    public static GameObject spider;
+    public Text text;
+    public Button resetButton;
+
+    //private static List<Path> path;
+    private static List<List<Path>> paths = new List<List<Path>>();
+    private static Vector3 localscale;
+    private GameObject spider;
     private static int count;
     private static float speed;
     private int slowAnim;
 
-    public static void setPath(List<Path> newPath, Vector3 localScale)
+    public static void setPath(Ant[] ants, Vector3 localScale)
     {
-        path = newPath;
-        speed = localScale[0]/16;
-        spider.transform.position = new Vector3(-4.85f, 0.2f, 4.85f);
-        spider.transform.rotation = new Quaternion(0, 90, 0, 0);
-        spider.transform.localScale = new Vector3(0.05f, 0.05f, 0.05f);
+        foreach(Ant ant in ants)
+        {
+            paths.Add(ant.path);
+        }
+        paths.Sort(delegate(List<Path> p1, List<Path> p2) { return p1.Count.CompareTo(p2.Count); });
+        localscale = localScale;
     }
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         count = 0;
         slowAnim = 0;
         spider = Instantiate(SPIDER);
-        spider.transform.position = new Vector3(0, 0.2f, 0);
-        spider.transform.rotation = new Quaternion(0, 0, 0, 0);
-        spider.transform.localScale = new Vector3(0.05f, 0.05f, 0.05f);
+        spider.transform.SetParent(transform.parent);
+        ResetPosition();
+        resetButton.onClick.AddListener(ResetIteration);
     }
-	
-	// Update is called once per frame
-	void Update () {
-        if (path != null && slowAnim >= 6)
+    int countPath = 0;
+    // Update is called once per frame
+    void Update()
+    {
+        if (countPath >= paths.Count) return;
+
+
+        List<Path> path = paths[countPath];
+        if (count == path.Count)
+        {
+            ResetPosition();
+            count = 0;
+            countPath++;
+            if (countPath >= paths.Count) return;
+            path = paths[countPath];
+            speed = localscale.x / 16;
+        }
+        text.text = "Iteration : " + countPath.ToString();
+        
+        if (path != null && slowAnim >= 1)
         {
             slowAnim = 0;
-            if (count >= path.Count) return;
             switch (path[count].dir)
             {
                 case Dir.EAST:
@@ -65,24 +88,38 @@ public class AntController : MonoBehaviour {
                     switch (path[count].dir)
                     {
                         case Dir.EAST:
-                            spider.transform.Translate(new Vector3(-speed*5, 0, 0));
+                            spider.transform.Translate(new Vector3(-speed * 5f, 0, 0));
                             break;
                         case Dir.WEST:
-                            spider.transform.Translate(new Vector3(speed*5, 0, 0));
+                            spider.transform.Translate(new Vector3(speed * 5f, 0, 0));
                             break;
                         case Dir.NORTH:
-                            spider.transform.Translate(new Vector3(0, 0, speed*5));
+                            spider.transform.Translate(new Vector3(0, 0, speed * 5f));
                             break;
                         case Dir.SOUTH:
-                            spider.transform.Translate(new Vector3(0, 0, speed*5));
+                            spider.transform.Translate(new Vector3(0, 0, speed * 5f));
                             break;
                         default:
                             break;
                     }
                     break;
             }
+
             count++;
         }
         slowAnim++;
-	}
+    }
+
+    void ResetPosition()
+    {
+        spider.transform.position = new Vector3(-4.85f, 30.2f, 4.85f);
+        spider.transform.rotation = new Quaternion(0, -90, 0, 0);
+        spider.transform.localScale = new Vector3(0.05f, 0.05f, 0.05f);
+    }
+
+    public void ResetIteration()
+    {
+        countPath = 0;
+    }
 }
+
